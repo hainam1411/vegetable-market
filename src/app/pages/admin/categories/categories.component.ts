59616bfe68afb6593777df3029c353e05657cf7e
hainam1411 <hainam141199@gmail.com>
2025-02-07 17:16:51 +0700

    categories list

--- src/app/pages/admin/categories/categories.component.ts
+++ src/app/pages/admin/categories/categories.component.ts
@@ -1,12 +1,24 @@
 import { Component } from '@angular/core';
+import {ProductService} from '../../../services/product/product.service';
+import {map, Observable} from 'rxjs';
+import { CommonModule} from '@angular/common';
 
 @Component({
   selector: 'app-categories',
-  imports: [],
+  imports: [
+    CommonModule
+  ],
   templateUrl: './categories.component.html',
   standalone: true,
   styleUrl: './categories.component.css'
 })
 export class CategoriesComponent {
-
+  products$: Observable<any>
+constructor(private productservice: ProductService) {
+    this.products$ = this.productservice.getCategory().pipe(
+      map((item:any)   => {
+        return item.data;
+      })
+    );
+}
 }
--- src/app/pages/admin/layout/layout.component.ts
+++ src/app/pages/admin/layout/layout.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
-import {RouterOutlet} from '@angular/router';
+import {RouterLink, RouterOutlet} from '@angular/router';
 
 @Component({
   selector: 'app-layout',
   imports: [
-    RouterOutlet
+    RouterOutlet,
+    RouterLink
   ],
   templateUrl: './layout.component.html',
   standalone: true,
