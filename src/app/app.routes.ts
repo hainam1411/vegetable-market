6c92d39d80de18c7b326b9a681b5b04ae2770689
hainam1411 <hainam141199@gmail.com>
2025-02-07 17:03:06 +0700

    categories list

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,12 +2,17 @@ import { Routes } from '@angular/router';
 import {LoginComponent} from './pages/admin/login/login.component';
 import {LayoutComponent} from './pages/admin/layout/layout.component';
 import {ProductComponent} from './pages/admin/product/product.component';
+import {CategoriesComponent} from './pages/admin/categories/categories.component';
+import {LandingComponent} from './pages/website/landing/landing.component';
 
 export const routes: Routes = [
   {path: '', redirectTo:'login', pathMatch:'full'},
-  {path: '', component: LoginComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'shop', component: LandingComponent},
   {path: '', component: LayoutComponent, children: [
-      {path: 'products', component: ProductComponent}
+      {path: 'products', component: ProductComponent},
+      {path: 'categories', component: CategoriesComponent},
+
 
     ]}
 ];
